b1a51479228d12f22202f1005e589c4274a9f7ee
Joel Kim <kim.dohhyoung@gmail.com>
2019-01-18 18:18:17 +0900

    rename locales -> locale

---
+++ create_ghpages.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+git add . -A
+git commit -m "automatic commit"
+git push origin korean
+
+git checkout -B gh-pages
+git rebase korean
+
+touch .nojekyll
+
+echo '!_build/' >> .gitignore
+echo '!env/' >> .gitignore
+
+cd docs/docsite
+make generate_rst
+sphinx-build -j auto -D language='ko' rst _build
+cd ../..
+
+git add . -A
+git commit -m "build"
+git push -f origin gh-pages
+git reset --hard HEAD
+git clean -f
+git checkout korean
+
+source deactivate
